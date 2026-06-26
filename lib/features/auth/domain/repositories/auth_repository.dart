bdbaf087329444dd3399a23fafeebba6131b6119
baslan <baslan.sau@gmail.com>
2026-06-26 15:36:58 +0700

    feat: add user entity and auth repository contract

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,1 +1,11 @@
-abstract class AuthRepository { }
+import 'package:dartz/dartz.dart';
+import 'package:trado_app/core/errors/failures.dart';
+import 'package:trado_app/features/auth/domain/entities/user.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, User>> signIn(String email, String password);
+  Future<Either<Failure, Unit>> signUp(
+      String email, String password, String username);
+  Future<Either<Failure, Unit>> signOut();
+  Future<Either<Failure, User>> getCurrentUser();
+}
